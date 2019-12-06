7322095ca4ef1f059fa58dba3e47a82fa03833f5
SkynetNote <mcjoi@naver.com>
2019-12-06 14:09:42 +0900

    20191206

--- lib/mypackage.dart
+++ lib/mypackage.dart
@@ -3,3 +3,4 @@ library mypackage;
 export './src/color.dart';
 export './src/icon.dart';
 export './src/wordlist.dart';
+export './src/textWidget.dart';
---
+++ lib/src/textWidget.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Widget myTextWidget(String txt) {
+  return Text(txt);
+}

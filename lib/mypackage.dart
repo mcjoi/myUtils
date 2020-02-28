9fd6da17a1f99a5f90da7f374b5602e5c0164790
mcjoi <mcjoi@naver.com>
2020-02-28 09:49:37 +0900

    20200228_2

--- lib/mypackage.dart
+++ lib/mypackage.dart
@@ -4,3 +4,39 @@ export './src/color.dart';
 export './src/icon.dart';
 export './src/wordlist.dart';
 export './src/textWidget.dart';
+
+/* 
+
+[color.dart]
+6자리 숫자로 구성된 hexcode로 컬러를 불러옵니다. 
+포토샵 컬러픽커를 사용할 수 있습니다.
+실행함수 : parseColor()
+
+[icon.dart]
+아이콘을 랜덤으로 불러옵니다.
+실행함수 : myRandomIcon()
+
+[wordlist.dart]
+랜덤워드 패키지로 랜덤단어를 발생시킵니다.
+리스트가 반환됩니다.
+
+실행함수 : myRandomWords(int, bool)
+int : 개수
+bool : true 이면 명사, false이면 두단어짜리 반환
+
+
+[textWidget.dart]
+이건 실험용입니다. 
+
+
+
+***************불러오기***************
+
+import 'package:mypackage/mypackage.dart';
+
+mypackage:
+    git:
+      url: https://github.com/mcjoi/myUtils.git
+
+
+*/

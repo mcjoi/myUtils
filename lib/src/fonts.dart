5f2a4bab6d2d7f97a3a2d621bef30a41ba35b3c1
bong <mcjoi@naver.com>
2022-09-14 11:57:22 +0900

    20220914

---
+++ lib/src/fonts.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MeFontFamily {
+  static List<String> fontlist = ['coo', 'cen', 'era'];
+}
+
+Widget myTextWidgets(
+    {required String fontFamilyname,
+    required String txt,
+    double? fontSize,
+    Color? fontColor}) {
+  TextStyle ts = TextStyle(
+      fontFamily: fontFamilyname, fontSize: fontSize, color: fontColor);
+
+  if (MeFontFamily.fontlist.contains(fontFamilyname)) {
+    return Text(
+      txt,
+      style: ts,
+    );
+  } else
+    return Text(txt);
+}
